d5e7ebe6c8976d1c6d7247b170935192ad94b311
Orbit X <153139012+0rbitX@users.noreply.github.com>
2024-12-19 22:37:32 +0000

    Finished add and add multiple

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function returnCallerLine() {
     return callerLine;
 }
 
-function addLibrary(link,name,Wname,callback){
+function addLibrary(link,name,Wname){
     const script = document.createElement("script");
     script.src = link;
     script.type = "text/javascript";
@@ -16,21 +16,138 @@ function addLibrary(link,name,Wname,callback){
         
     };
     script.onerror = () => {
-        console.error("Error loading "+name+". Please inspect your code for errors.");
+        console.error("Error loading "+name+returnCallerLine());
     };
     document.head.appendChild(script);
 }
 class Port{
-    static add(library,callback){
+    static add(library,customLink,name,wName){
         switch(library){
             case "jquery":    
             addLibrary("https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js","jQuery","$",callback)   
             break;
+            case "lodash":    
+            addLibrary("https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js","Loadash","_")   
+            break;
+            case "moment.js":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js","Moment.js","moment")
+            break;
+            case "axios":    
+            addLibrary("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js","Axios","axios")   
+            break;
+            case "chart":    
+            addLibrary("https://cdn.jsdelivr.net/npm/chart.js","Chart.js","Chart")   
+            break;
+            case "three":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/three.js/r152/three.min.js","Three.js","THREE")   
+            break;
+            case "D3":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/d3/7.8.5/d3.min.js","D3.js","d3")   
+            break;
+            case "Anime":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js","Anime","anime")   
+            break;
+            case "bootstrap":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js","Bootstrap","bootstrap")   
+            break;
+            case "vue":    
+            addLibrary("https://cdn.jsdelivr.net/npm/vue@3.3.4/dist/vue.global.js","Vue","vue")   
+            break;
+            case "react":    
+            addLibrary("https://unpkg.com/react@17/umd/react.development.js","React","React")   
+            break;
+            case "reactDOM":    
+            addLibrary("https://unpkg.com/react-dom@17/umd/react-dom.development.js","ReactDOM","ReactDOM")   
+            break;
+            case "popper":    
+            addLibrary("https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js","Popper","Popper")   
+            break;
+            case "backbone":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.1/backbone-min.js","Backbone","Backbone")   
+            break;
+            case "swiper":    
+            addLibrary("https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js","Swiper.js","swiper")   
+            break;
+            case "modernizr":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/modernizr/3.12.0/modernizr.min.js","Modernizr","Modernizr")   
+            break;
+            case "toastify":    
+            addLibrary("https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.js","Toastify","Toastify")   
+            break;
+            case "babel":    
+            addLibrary("https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js","Babel","Babel")   
+            break;
+            case "custom":
+            addLibrary(customLink,name,wName)
+                break;
+            
             default:
                 console.error("Error loading "+library+" "+returnCallerLine())
         }
         
             }
+    static addMultiple(libraries){
+        for(var i =0;i<libraries.length;i++){
+            switch(libraries[i]){
+                case "jquery":    
+                addLibrary("https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js","jQuery","$",callback)   
+                break;
+                case "lodash":    
+                addLibrary("https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js","Loadash","_")   
+                break;
+                case "moment.js":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js","Moment.js","moment")
+                break;
+                case "axios":    
+                addLibrary("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js","Axios","axios")   
+                break;
+                case "chart":    
+                addLibrary("https://cdn.jsdelivr.net/npm/chart.js","Chart.js","Chart")   
+                break;
+                case "three":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/three.js/r152/three.min.js","Three.js","THREE")   
+                break;
+                case "D3":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/d3/7.8.5/d3.min.js","D3.js","d3")   
+                break;
+                case "Anime":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js","Anime","anime")   
+                break;
+                case "bootstrap":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js","Bootstrap","bootstrap")   
+                break;
+                case "vue":    
+                addLibrary("https://cdn.jsdelivr.net/npm/vue@3.3.4/dist/vue.global.js","Vue","vue")   
+                break;
+                case "react":    
+                addLibrary("https://unpkg.com/react@17/umd/react.development.js","React","React")   
+                break;
+                case "reactDOM":    
+                addLibrary("https://unpkg.com/react-dom@17/umd/react-dom.development.js","ReactDOM","ReactDOM")   
+                break;
+                case "popper":    
+                addLibrary("https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js","Popper","Popper")   
+                break;
+                case "backbone":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.1/backbone-min.js","Backbone","Backbone")   
+                break;
+                case "swiper":    
+                addLibrary("https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js","Swiper.js","swiper")   
+                break;
+                case "modernizr":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/modernizr/3.12.0/modernizr.min.js","Modernizr","Modernizr")   
+                break;
+                case "toastify":    
+                addLibrary("https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.js","Toastify","Toastify")   
+                break;
+                case "babel":    
+                addLibrary("https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js","Babel","Babel")   
+                break;
+                default:
+                    console.error("Error loading "+library+" "+returnCallerLine())
+            }
+        }
+    }
 }
 class Baseplate{
     
