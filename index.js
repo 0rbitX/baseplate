6d2a03f1771f83909474338368cdcc9ae6742dfc
Orbit X <153139012+0rbitX@users.noreply.github.com>
2024-12-19 21:30:37 +0000

    working ot port

--- index.js
+++ index.js
@@ -4,41 +4,30 @@ function returnCallerLine() {
     const callerLine = stackLines[2]; // The third line in the stack is the caller
     return callerLine;
 }
-function pollForLibrary(libName,WindowName,callback,interval,timeout){
-    var startTime = Date.now()
-    var interval = interval || 5000;
-    var timeout = timeout || 50;
-    (function checkLibrary() {
-        if (window[WindowName]) {
-          callback();
-          console.log('Port successfully loaded ${libName}') // Call the callback once the library is available
-        } else if (Date.now() - startTime < timeout) {
-          setTimeout(checkLibrary, interval); // Retry after the specified interval
-        } else {
-          console.error(`${libName} did not load within the timeout period.`);
-        }
-      })();
+
+function addLibrary(link,name,Wname,callback){
+    const script = document.createElement("script");
+    script.src = link;
+    script.type = "text/javascript";
+    script.onload = () => {
+    console.log("hi")
+    callback()
+        
+        
+    };
+    script.onerror = () => {
+        console.error("Error loading "+name+". Please inspect your code for errors.");
+    };
+    document.head.appendChild(script);
 }
 class Port{
     static add(library,callback){
         switch(library){
-            case "jquery":
-                const script = document.createElement("script");
-                script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
-                script.type = "text/javascript";
-                script.onload = () => {
-                    pollForLibrary("Jquery","jQuery",callback)
-                    
-                    
-                };
-                script.onerror = () => {
-                    console.error("Error loading Jquery. Please inspect your code for errors.");
-                };
-                document.head.appendChild(script);
-                
+            case "jquery":    
+            addLibrary("https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js","jQuery","$",callback)   
             break;
             default:
-                console.log("Error loading library "+returnCallerLine())
+                console.error("Error loading "+library+" "+returnCallerLine())
         }
         
             }
